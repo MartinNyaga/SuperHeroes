388ecbf47cfa8512b1bd4d6a4772613bfc646c13
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-10-01 01:40:12 +0300

    seeded data

--- app/seed.py
+++ app/seed.py
@@ -1,35 +1,70 @@
-puts "🦸‍♀️ Seeding powers..."
-Power.create([
-  { name: "super strength", description: "gives the wielder super-human strengths" },
-  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
-  { name: "elasticity", description: "can stretch the human body to extreme lengths" }
-])
+from faker import Faker
+import random
+from app import app
 
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
+from models import db, Hero, Power, HeroPower
 
-puts "🦸‍♀️ Adding powers to heroes..."
+with app.app_context():
 
-strengths = ["Strong", "Weak", "Average"]
-Hero.all.each do |hero|
-  rand(1..3).times do
-    # get a random power
-    power = Power.find(Power.pluck(:id).sample)
+    fake = Faker()
 
-    HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
-  end
-end
-
-puts "🦸‍♀️ Done seeding!"
+    Hero.query.delete()
+    Power.query.delete()
+    HeroPower.query.delete()
+    db.session.commit()
+    
+    print("🦸‍♀️ Seeding powers...")
+    
+    powers_data = [
+      { "name": "super strength", "description": "gives the wielder super-human strengths" },
+      { "name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed" },
+      { "name": "super human senses", "description": "allows the wielder to use her senses at a super-human level" },
+      { "name": "elasticity", "description": "can stretch the human body to extreme lengths" }
+    ]
+    
+    for power in powers_data:
+      data = Power(**power)
+      db.session.add(data)
+    db.session.commit()
+    
+    
+    print("🦸‍♀️ Seeding heroes...")
+    
+    heroes_data = [
+      { "name": "Kamala Khan", "super_name": "Ms. Marvel" },
+      { "name": "Doreen Green", "super_name": "Squirrel Girl" },
+      { "name": "Gwen Stacy", "super_name": "Spider-Gwen" },
+      { "name": "Janet Van Dyne", "super_name": "The Wasp" },
+      { "name": "Wanda Maximoff", "super_name": "Scarlet Witch" },
+      { "name": "Carol Danvers", "super_name": "Captain Marvel" },
+      { "name": "Jean Grey", "super_name": "Dark Phoenix" },
+      { "name": "Ororo Munroe", "super_name": "Storm" },
+      { "name": "Kitty Pryde", "super_name": "Shadowcat" },
+      { "name": "Elektra Natchios", "super_name": "Elektra" }
+    ]
+    
+    for hero in heroes_data:
+      data = Hero(**hero)
+      db.session.add(data)
+      
+    db.session.commit()
+    
+    print("🦸‍♀️ Adding powers to heroes...")
+    
+    strengths = ["Strong", "Weak", "Average"]
+    
+    powers = Power.query.all()
+    heroes = Hero.query.all()
+    
+    for hero in heroes:
+      for i in range(random.randint(1, 7)):
+        power = random.choice(powers)
+        strength = random.choice(strengths)
+        
+        hero_power = HeroPower(herro_id=hero.id, power_id=power.id, strength= strength)
+        db.session.add(hero_power)
+        
+    db.session.commit()
+    
+    print("🦸‍♀️ Done seeding!")
+    
